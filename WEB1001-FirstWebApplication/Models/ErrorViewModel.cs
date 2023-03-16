ab9ae9c1dd6a9cf2433879c5080e449e6b943337
Deepshika Ghale <58839663+DeepshikaGhale@users.noreply.github.com>
2023-03-15 21:10:33 -0400

    Initial Commit

---
+++ WEB1001-FirstWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WEB1001_FirstWebApplication.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
